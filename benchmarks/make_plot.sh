0188aa07d0c7d449d6f24dc943a4025fce8f809c
Andy <amueller@ais.uni-bonn.de>
2013-09-24 14:51:24 +0200

    add plot script to be save

---
+++ benchmarks/make_plot.sh
@@ -0,0 +1,10 @@
+plot_learning.py letters_small_1-slack-cache.pickle letters_small_1-slack.pickle  letters_small_pegasos.pickle letters_small_SZLJSP.pickle letters_small_n-slack.pickle letters_small_n-slack-every.pickle --save letters_small_log.pdf&
+plot_learning.py letters_small_1-slack-cache.pickle letters_small_1-slack.pickle  letters_small_pegasos.pickle letters_small_SZLJSP.pickle letters_small_n-slack.pickle letters_small_n-slack-every.pickle --save letters_small_log_time.pdf --time&
+plot_learning.py letters_big_1-slack-cache.pickle letters_big_1-slack.pickle letters_big_pegasos.pickle letters_big_SZLJSP.pickle letters_big_n-slack.pickle --save letters_big_log.pdf&
+plot_learning.py letters_big_1-slack-cache.pickle letters_big_1-slack.pickle letters_big_pegasos.pickle letters_big_SZLJSP.pickle letters_big_n-slack.pickle --save letters_big_log_time.pdf --time&
+plot_learning.py scene_tree_1-slack-cache.pickle scene_tree_1-slack.pickle  scene_tree_pegasos.pickle scene_tree_SZLJSP.pickle scene_tree_n-slack.pickle scene_tree_n-slack-every.pickle --save scene_tree_log.pdf&
+plot_learning.py scene_tree_1-slack-cache.pickle scene_tree_1-slack.pickle  scene_tree_pegasos.pickle scene_tree_SZLJSP.pickle scene_tree_n-slack.pickle scene_tree_n-slack-every.pickle --save scene_tree_log_time.pdf --time&
+plot_learning.py scene_full_1-slack-cache.pickle scene_full_1-slack.pickle  scene_full_pegasos.pickle scene_full_SZLJSP.pickle scene_full_n-slack.pickle scene_full_n-slack-every.pickle --save scene_full_log.pdf&
+plot_learning.py scene_full_1-slack-cache.pickle scene_full_1-slack.pickle  scene_full_pegasos.pickle scene_full_SZLJSP.pickle scene_full_n-slack.pickle scene_full_n-slack-every.pickle --save scene_full_log_time.pdf --time&
+plot_learning.py yeast_tree_1-slack-cache.pickle yeast_tree_1-slack.pickle  yeast_tree_pegasos.pickle yeast_tree_SZJLSP.pickle yeast_tree_n-slack.pickle --save yeast_tree_log.pdf&
+plot_learning.py yeast_tree_1-slack-cache.pickle yeast_tree_1-slack.pickle  yeast_tree_pegasos.pickle yeast_tree_SZJLSP.pickle yeast_tree_n-slack.pickle --save yeast_tree_log_time.pdf --time&
